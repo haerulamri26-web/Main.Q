5943bf94cfdcd73e3f44d6e83250a09396a7013f
haerulamri26-web <haerulamri26@gmail.com>
2026-02-03 21:41:27 +0800

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = 'https://mainq.netlify.app'; 
+  const siteUrl = 'https://mainq.my.id'; 
  
   return {
     rules: {
